e22bdd9a91aa8a94c85fca3f954760ed5a4620dd
krajcovic <krajcovic@dkr-workstation.monetplus.cz>
2016-04-19 11:16:08 +0200

    Update

--- socat-acm0.sh
+++ socat-acm0.sh
@@ -2,13 +2,13 @@
 
 case "$1" in
   start)
-    echo "Starting noip"
+    echo "Starting socat-acm"
     # run application you want to start
     #/usr/local/bin/noip2
     socat -d -d -d -D TCP-LISTEN:27644,forever /dev/ttyACM0,echo=0,raw,unlink-close=0 > /var/log/socat-acm-27644.log 2>&1
     ;;
   stop)
-    echo "Stopping noip"
+    echo "Stopping socat-acm"
     # kill application you want to stop
     killall socat
     ;;
